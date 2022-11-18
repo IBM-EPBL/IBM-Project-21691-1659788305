be9270f37f7e018eca0a8a6dfe11ce9dd988610d
Harish <74132905+questhunter@users.noreply.github.com>
2022-11-18 22:08:21 +0530

    Update SendMail.py

--- Project Development Phase/Sprint 3/Model/SendMail.py
+++ Project Development Phase/Sprint 3/Model/SendMail.py
@@ -1,34 +1,29 @@
-# import sendgrid
-# import os
-# from sendgrid.helpers.mail import *
-
-API_KEY='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'
-
-# sg = sendgrid.SendGridAPIClient(api_key=os.environ.get(API_KEY))
-# from_email = Email("test@example.com")
-# to_email = To("clashharish@gmail.com")
-# subject = "Sending with SendGrid is Fun"
-# content = Content("text/plain", "and easy to do anywhere, even with Python")
-# mail = Mail(from_email, to_email, subject, content)
-# response = sg.client.mail.send.post(request_body=mail.get())
-# print(response.status_code)
-# print(response.body)
-# print(response.headers)
-
-import os
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-
-message = Mail(
-    from_email='trapremix21@gmail.com',
-    to_emails='clashharish@gmail.com',
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
-try:
-    sg = SendGridAPIClient(API_KEY)
-    response = sg.send(message)
-    print('new',response.status_code)
-    print(response.body)
-    print(response.headers)
-except Exception as e:
-    print(e)
+from flask import Flask
+from flask_mail import Mail, Message
+   
+app = Flask(_name_)
+mail = Mail(app) # instantiate the mail class
+   
+# configuration of mail
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'ajithkumarc67@gmail.com'
+app.config['MAIL_PASSWORD'] = 'oaijanvwlpaixuby'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+   
+# message object mapped to a particular URL ‘/’
+@app.route("/")
+def index():
+   msg = Message(
+                'Alert',
+                sender ='ajithkumarc67@gmail.com',
+                recipients = ['kajithv4@gmail.com']
+               )
+   msg.body = 'Hello Flask message sent from Flask-Mail'
+   mail.send(msg)
+   return 'Sent'
+   
+if _name_ == '_main_':
+   app.run(debug = True)
